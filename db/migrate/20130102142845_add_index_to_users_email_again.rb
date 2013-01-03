df8ec797fc9fff232f231f00145cec209b1f702e
Martin Barksten <martin.barksten@gmail.com>
2013-01-03 15:51:57 +0100

    Make a basic User model (including secure passwords)

---
+++ db/migrate/20130102142845_add_index_to_users_email_again.rb
@@ -0,0 +1,5 @@
+class AddIndexToUsersEmailAgain < ActiveRecord::Migration
+  def change
+  	add_index :users, :email, unique: true
+  end
+end
--- spec/support/utilites.rb
+++ spec/support/utilites.rb
@@ -1,5 +1,3 @@
-include ApplicationsHelper
-
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
